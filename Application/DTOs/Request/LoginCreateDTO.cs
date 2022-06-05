899135078e0f5b1b03d576aab5d5c27bddb5e6da
Matheus Silva <matheus.silva@ferreiracosta.com.br>
2022-06-05 12:52:37 -0300

    Feat: Sera adicionado validacoes de DTO

--- Application/DTOs/Request/LoginCreateDTO.cs
+++ Application/DTOs/Request/LoginCreateDTO.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Application.DTOs.Request
 {
     public class LoginCreateDTO
     {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
         public string UserName { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(20)]
         public string Password { get; set; }
     }
 }
--- Application/DTOs/Request/UserCreateDTO.cs
+++ Application/DTOs/Request/UserCreateDTO.cs
@@ -1,12 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Application.DTOs.Request;
 
 public class UserCreateDTO
 {
+    [Required]
     public string Name { get; set; }
+
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+
+    [Phone]
     public string Phone { get; set; }
+
+    [Required]
     public string CPF { get; set; }
+
+    [Required]
     public DateTime BirthDate { get; set; }
+
+    [Required]
     public string MotherName { get; set; }
+
+    [Required]
     public LoginCreateDTO Login { get; set; }
 }
