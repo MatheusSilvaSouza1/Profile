2f8cecf2b1b334187869c3f72614a480ba7b2132
Matheus Silva <matheus.silva@ferreiracosta.com.br>
2022-06-05 00:27:58 -0300

    Feat: Sera criado a camada de infra

---
+++ Infra/WriteContext.cs
@@ -0,0 +1,16 @@
+
+using Domain.SeedWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra
+{
+    public class WriteContext : DbContext, IUnitOfWork
+    {
+        public WriteContext(DbContextOptions<WriteContext> options) : base(options) { }
+
+        public Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
