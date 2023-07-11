602fdff3d189b4273a900c542ff84bde09019a84
Matheus Silva <matheus.silva@ferreiracosta.com.br>
2023-07-10 21:51:32 -0300

    Sera renomeado o controller de usuario para atender o padrao de nomenclatura rest

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Commands;
+using Domain.DTOs.Request;
+using Domain.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IMediator _handler;
+    private readonly IUserRepository _userRepository;
+
+    public UsersController(IMediator handler, IUserRepository userRepository)
+    {
+        _handler = handler;
+        _userRepository = userRepository;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(UserCreateDTO user)
+    {
+        var result = await _handler.Send(new CreateUserCommand(user));
+        if (!result.ValidationResult.IsValid)
+        {
+            return BadRequest(result.ValidationResult);
+        }
+
+        return Created(string.Empty, result.Data);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var users = await _userRepository.FindAllUser();
+        if (users.Any())
+        {
+            return Ok(users);
+        }
+
+        return NoContent();
+    }
+}
