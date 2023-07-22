04938c65a64ae5e29a4497bb884580e1d80f8fbe
Matheus Silva <matheus.silva@ferreiracosta.com.br>
2023-07-21 23:35:56 -0300

    Sera criada a migrations que remove a tabela de login e cria os campos na tabela de user

--- Infra/Migrations/20230722023301_remove-login-table.Designer.cs
+++ Infra/Migrations/20230722023301_remove-login-table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infra;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infra.Migrations
 {
     [DbContext(typeof(WriteContext))]
-    partial class WriteContextModelSnapshot : ModelSnapshot
+    [Migration("20230722023301_remove-login-table")]
+    partial class removelogintable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.5");
